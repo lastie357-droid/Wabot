28b68cb4ce92d23172fba9e219bfe907787cd6ca
trekkermoja1 <trekkermoja1@gmail.com>
2026-03-09 07:03:27 +0000

    TREKKER

--- bot/instance.js
+++ bot/instance.js
@@ -9,6 +9,7 @@ const chalk = {
   blue: (text) => `\x1b[34m${text}\x1b[0m`,
   green: (text) => `\x1b[32m${text}\x1b[0m`,
   red: (text) => `\x1b[31m${text}\x1b[0m`,
+  redBright: (text) => `\x1b[31m\x1b[1m${text}\x1b[0m`,
   yellow: (text) => `\x1b[33m${text}\x1b[0m`,
   cyan: (text) => `\x1b[36m${text}\x1b[0m`,
   magenta: (text) => `\x1b[35m${text}\x1b[0m`,
